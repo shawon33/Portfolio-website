e111944a110a30c8650dca9cd8b374bcc9087497
shawon33 <iamcoming115@gmail.com>
2023-09-28 21:32:37 +0600

    i change img which one i use scroll section

--- src/Component/Section/Scroll.jsx
+++ src/Component/Section/Scroll.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { FaArrowDown } from "react-icons/fa6";
 
+import img from "../../../public/section-1/alba-paris-pastels.gif"
 const Scroll = () => {
   return (
     <div className="text-center md:my-16 ">
-        <p>scroll</p>
-       <FaArrowDown className="mx-auto  text-6xl  "></FaArrowDown>
+        <img src={img} alt="" className="mx-auto w-36 h-auto" />
     </div>
   );
 };
